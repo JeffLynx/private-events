324c9bd1c73ad351daa3501e5cc32968ebed0195
JeffLynx <55367715+JeffLynx@users.noreply.github.com>
2025-12-16 00:57:17 +0100

    Events and users set up

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :created_events,
+    foreign_key: :creator_id,
+    class_name: "Event"
+end
